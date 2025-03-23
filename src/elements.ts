6745e95ff195567db44ef873622658f01e9fec78
IsmoilQaxramonov <frontqaxramonov@gmail.com>
2025-03-23 16:20:26 +0500

    done home and shop

---
+++ src/elements.ts
@@ -0,0 +1,3 @@
+export const featured = document.querySelector(".featured") as HTMLDivElement;
+export const recommended = document.querySelector(".recommended") as HTMLDivElement;
+export const boxes = document.querySelector(".boxes") as HTMLDivElement;
